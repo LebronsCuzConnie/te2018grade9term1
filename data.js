5db3215d8d02d42b2c88ba799148361769b25bd6
LebronsCuzConnie <constance.rosemond@csedge.org>
2018-10-06 15:45:41 -0400

    Completed Chllenge One

--- data.js
+++ data.js
@@ -1,5 +1,23 @@
 // Example
-let message = "Hello World!";
-console.log(message);
+let name = "Constance";
+console.log(name);
+let nickName = "Connie";
+console.log(typeof name); 
+let food = "Lobster Tail";
+console.log(food);
+let age = 14;
+console.log(age);
+let siblings = 3;
+console.log(siblings);
+let familyMembers = 5;
+console.log(familyMembers);
 
-// Type your code below!
+let hasBrother = false;
+console.log(hasBrother);
+let hasSister = true;
+console.log(hasSister);
+
+const READLINE = require("readline-sync");
+let userInput = READLINE.question("Type anything and I'll repeat it back!");
+console.log('hey');
+
