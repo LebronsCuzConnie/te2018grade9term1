00a6d830989d774d0b539d92cf104f28b1f32d26
LebronsCuzConnie <constance.rosemond@csedge.org>
2018-10-20 14:35:52 -0400

    Finished changes.js

--- change.js
+++ change.js
@@ -2,6 +2,31 @@
 const READLINE = require("readline-sync");
 
 // fill in object below with all relevant properties (see instructions)
-let changeMachine = {};
+console.log("bLeep blOop! i'm a CHanGE macHINe!")
 
-// write code below that simulates the change machine in action!
+let inputCents = READLINE.question("Please enter cents as a positive integer: ");
+console.log(`${inputCents} cents makes:`);
+let quarters = Math.floor(inputCents/25);
+let dimes = Math.floor(inputCents%25/10)
+let nickels = Math.floor(inputCents%10/5)
+let pennies = Math.floor(dimes%5/1)
+
+
+let changeMachine = {
+	cents : inputCents,
+	quarters : quarters,
+	dimes : dimes,
+	nickel : nickels,
+	pennies : pennies,
+	greeting : "bLeep blOop! i'm a CHanGE macHINe!",
+	farewell : "BLOop BleEp! i Hope yOU aRE SATiSfieD wITh thiS SerVicE!"
+};
+
+console.log(`${quarters} quarters`);
+console.log(`${dimes} dimes`);
+console.log(`${nickels} nickels`);
+console.log(`${pennies} pennies`);
+
+console.log("BLOop BleEp! i Hope yOU aRE SATiSfieD wITh thiS SerVicE!");
+
+
