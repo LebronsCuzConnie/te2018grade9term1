ee2371eaf40850d27b3a852201fb86aaaeb4aca3
LebronsCuzConnie <constance.rosemond@csedge.org>
2018-10-20 14:57:16 -0400

    REVISED Finished change.js

--- change.js
+++ change.js
@@ -6,26 +6,29 @@ console.log("bLeep blOop! i'm a CHanGE macHINe!")
 
 let inputCents = READLINE.question("Please enter cents as a positive integer: ");
 console.log(`${inputCents} cents makes:`);
-let quarters = Math.floor(inputCents/25);
-let dimes = Math.floor(inputCents%25/10)
-let nickels = Math.floor(inputCents%10/5)
-let pennies = Math.floor(dimes%5/1)
+let quarters = Math.floor((inputCents)/25);
+let remainder = inputCents%25;
+let dimes = Math.floor(remainder/10);
+remainder = remainder%10;
+let nickels = Math.floor(remainder/5);
+remainder = remainder%5;
+let pennies = Math.floor(remainder/1);
 
 
 let changeMachine = {
 	cents : inputCents,
 	quarters : quarters,
 	dimes : dimes,
-	nickel : nickels,
+	nickels : nickels,
 	pennies : pennies,
 	greeting : "bLeep blOop! i'm a CHanGE macHINe!",
 	farewell : "BLOop BleEp! i Hope yOU aRE SATiSfieD wITh thiS SerVicE!"
 };
 
-console.log(`${quarters} quarters`);
-console.log(`${dimes} dimes`);
-console.log(`${nickels} nickels`);
-console.log(`${pennies} pennies`);
+console.log(`${changeMachine.quarters} quarters`);
+console.log(`${changeMachine.dimes} dimes`);
+console.log(`${changeMachine.nickels} nickels`);
+console.log(`${changeMachine.pennies} pennies`);
 
 console.log("BLOop BleEp! i Hope yOU aRE SATiSfieD wITh thiS SerVicE!");
 
