c4cdc0cfcbb77baf922fd3b09d1b136d8855b2a1
LebronsCuzConnie <constance.rosemond@csedge.org>
2018-10-20 11:57:25 -0400

    Finished object.js

---
+++ object.js
@@ -0,0 +1,17 @@
+let spy = {
+	isSpy : true,
+	firstName: "Constance",
+	lastName: "Rosemond",
+	nickName : "LeBrons Cuz"
+};
+
+console.log(`I'm ${spy.nickName}: "It's ${spy.lastName}, ${spy.firstName} ${spy.lastName}"`);
+
+let spyEnemy = {
+	isSpy : true,
+	firstName: "Maya",
+	lastName: "Whites",
+	nickName : "Stephens Cuz"
+};
+
+console.log(`I'm ${spyEnemy.nickName}: "It's ${spyEnemy.lastName}, ${spyEnemy.firstName} ${spyEnemy.lastName}"`)
